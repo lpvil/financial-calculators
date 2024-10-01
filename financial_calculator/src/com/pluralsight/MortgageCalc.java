e8d9e5c8d6475b5d572e319ac8b02d5fa705f48c
lpvil <lpvillalone@gmail.com>
2024-10-01 12:18:23 -0700

    updated

---
+++ financial_calculator/src/com/pluralsight/MortgageCalc.java
@@ -0,0 +1,45 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+//calculate how much monthly payments for loan
+//how much interest paid on life of loan
+//takes input of principal, interest rate, loan length
+//display total monthly payment and total interest paid
+public class MortgageCalc{
+    static Scanner scanner= new Scanner(System.in);
+    public static void main(String[] args) {
+        int loanlength= loanLength();
+        double loanamount=loanAmount();
+       double interestrate=interestRate();
+
+        int totalpayments= loanlength*12;
+        double monthlyIntR=interestrate/12;
+        double monthlypayment= loanamount * (interestrate * Math.pow(1+interestrate,totalpayments))
+                /((Math.pow(1+interestrate,totalpayments))-1);
+        monthly(monthlyIntR, monthlypayment, totalpayments);
+
+
+    }
+    public static int loanLength(){
+        System.out.println("how long would you need loan for?: ");
+        int loanL= scanner.nextInt();
+        scanner.nextLine();
+        return loanL;
+    }
+    public static double loanAmount(){
+        System.out.println("how much would you like to borrow?: ");
+        double loan= scanner.nextDouble();
+        return loan;
+    }
+
+    public static double interestRate(){
+        System.out.println("what interest rate are you looking for?: ");
+        double intR= scanner.nextDouble();
+        return intR;
+    }
+    public static void monthly(Double monthlyIntR, Double monthlypayment, Integer totalpayments){
+        System.out.println("your monthly interest rate: " +monthlyIntR);
+        System.out.println("your monthly payment: " +monthlypayment);
+        System.out.println("your total payments when paid off: " +totalpayments);
+    }
+}
